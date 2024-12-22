6ee09abf55f0754ded40b9beac063ab4a1dd833f
Vladimir Kosimovsky <vladimir.kosimovsky@outlook.com>
2024-12-23 01:38:58 +0300

    hello part

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+
+#[tokio::main]
+async fn main() -> Result<(), Box<(dyn std::error::Error)>> {
+    let client = reqwest::Client::builder()
+        .build()?;
+
+    let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert("Content-Type", "application/json".parse()?);
+
+    let data = r#"{
+    "model": "llama3.2",
+    "prompt": "Why is the sky blue?",
+    "stream": false
+}"#;
+
+    let json: serde_json::Value = serde_json::from_str(&data)?;
+
+    let request = client.request(reqwest::Method::POST, "http://localhost:11434/api/generate")
+        .headers(headers)
+        .json(&json);
+
+    let response = request.send().await?;
+    let body = response.text().await?;
+
+    println!("{}", body);
+
+    Ok(())
+}
