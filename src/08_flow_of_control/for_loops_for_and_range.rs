892554db70eae0347919acd62670ffb0bf47befd
Vladimir Kosimovsky <vladimir.kosimovsky@outlook.com>
2025-05-16 08:48:44 +0300

    control flow

---
+++ src/08_flow_of_control/for_loops_for_and_range.rs
@@ -0,0 +1,14 @@
+fn main() {
+    for n in 1..100 {
+    // for n in 1..=100 {  
+        if n % 15 == 0 {
+            println!("Fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("Fizz");
+        } else if n % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+}
